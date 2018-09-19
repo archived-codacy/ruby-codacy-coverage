7ff617cc204815c6e4b1daab42ee705e516dc051
Mangesh Tamhankar <mtamhankar@idatainc.com>
2018-09-19 16:49:38 -0400

    add command for final reporter

--- lib/codacy/client.rb
+++ lib/codacy/client.rb
@@ -1,6 +1,7 @@
 require 'json'
-require "uri"
-require "net/https"
+require 'uri'
+require 'net/https'
+require 'codacy/git'
 
 module Codacy
   module ClientAPI
@@ -8,9 +9,7 @@ module Codacy
       logger.info('Preparing payload...')
       logger.debug(parsed_result)
 
-      project_token = ENV['CODACY_PROJECT_TOKEN']
-      codacy_base_api = ENV['CODACY_BASE_API_URL'] || 'https://api.codacy.com'
-      commit = Codacy::Git.commit_id
+      project_token, codacy_base_api, commit = get_parameters
       url = create_url(codacy_base_api, commit)
 
       result = parsed_result.to_json
@@ -37,7 +36,7 @@ module Codacy
       http.use_ssl = uri.scheme == "https"
       request["project_token"] = project_token
       request["Content-Type"] = "application/json"
-      request.body = content
+      request.body = content if content
       response = http.request(request)
 
       if [301, 302, 307].include? response.code.to_i and redirects > 0
@@ -52,9 +51,39 @@ module Codacy
       Codacy::Configuration.logger
     end
 
-    def self.create_url(codacy_base_api, commit)
+    def self.create_url(codacy_base_api, commit, final: false)
       commit = commit.gsub(/[^[:alnum:]]/, "")
-      codacy_base_api + '/2.0/coverage/' + commit + '/ruby'
+      if final
+        codacy_base_api + '/2.0/commit/' + commit + '/coverageFinal'
+      else
+        codacy_base_api + '/2.0/coverage/' + commit + '/ruby'
+      end
+    end
+
+    def self.notify_final
+      project_token, codacy_base_api, commit = get_parameters
+      url = create_url(codacy_base_api, commit, final: true)
+
+      if project_token.to_s == ''
+        puts 'Could not find Codacy API token, make sure you have it configured in your environment.'
+        false
+      elsif commit.to_s == ''
+        puts 'Could not find the current commit, make sure that you are running inside a git project.'
+        false
+      else
+        puts 'Posting Coverage Final Notice to ' + url
+        response = send_request(url, nil, project_token)
+
+        puts response
+        response.to_s.include? 'success'
+      end
+    end
+
+    def self.get_parameters
+      project_token = ENV['CODACY_PROJECT_TOKEN']
+      codacy_base_api = ENV['CODACY_BASE_API_URL'] || 'https://api.codacy.com'
+      commit = Codacy::Git.commit_id
+      return project_token, codacy_base_api, commit
     end
   end
 end
