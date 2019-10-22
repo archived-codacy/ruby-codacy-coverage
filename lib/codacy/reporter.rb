92347c6543d3ed212c993b9e18870bc72fe524bf
Lorenzo Gabriele <lorenzolespaul@gmail.com>
2019-10-22 15:01:26 +0200

    style: Use same convection for default parameters
    Delete a test

--- lib/codacy/reporter.rb
+++ lib/codacy/reporter.rb
@@ -2,7 +2,7 @@ require 'simplecov'
 
 module Codacy
   module Reporter
-    def self.start(profile = nil, partial: true)
+    def self.start(profile = nil, partial = true)
       SimpleCov.formatter = partial ? Codacy::Formatter::Partial : Codacy::Formatter
       SimpleCov.start(profile)
     end
